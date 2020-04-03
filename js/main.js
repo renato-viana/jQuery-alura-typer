352e1d12008c051f061f448f00046530301816ac
Renato Viana <renatoviana30@gmail.com>
2020-04-03 15:49:42 -0300

    GameOver com eventos - Evento focus e desabilitando um elemento com jQuery

--- js/main.js
+++ js/main.js
@@ -5,15 +5,25 @@ tamanhoFrase.text(numeroPalavras);
 
 var campo = $(".campo-digitacao");
 
-campo.on("input", function() {
-    var conteudo = campo.val(); //$(this).val();
-    var qtdPalavras = conteudo.split(/\s+/).length;
+campo.on("input", function () {
+  var conteudo = campo.val(); //$(this).val();
+  var qtdPalavras = conteudo.split(/\s+/).length;
 
-   $("#contador-palavras").text(qtdPalavras);
-   
-   //Expressão regular (/\s+/g) \s is the regex for "whitespace", and g is the "global" flag, meaning match ALL \s (whitespaces).
-   var qtdCaracteres =conteudo.replace(/\s+/g, '').length;
-   $("#contador-caracteres").text(qtdCaracteres);
-  
+  $("#contador-palavras").text(qtdPalavras);
+
+  //Expressão regular (/\s+/g) \s is the regex for "whitespace", and g is the "global" flag, meaning match ALL \s (whitespaces).
+  var qtdCaracteres = conteudo.replace(/\s+/g, "").length;
+  $("#contador-caracteres").text(qtdCaracteres);
 });
 
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function () {
+  var cronometroID = setInterval(function () {
+    tempoRestante--;
+    $("#tempo-digitacao").text(tempoRestante);
+    if (tempoRestante < 1) {
+      campo.attr("disabled", true);
+      clearInterval(cronometroID);
+    }
+  }, 1000);
+});
