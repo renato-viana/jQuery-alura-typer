8d50a202378a72d68f21baa6e105fca0392c743c
Renato Viana <renatoviana30@gmail.com>
2020-04-08 17:36:45 -0300

    Animações com jQuery

--- js/placar.js
+++ js/placar.js
@@ -1,3 +1,5 @@
+$("#botao-placar").click(mostraPlacar);
+    
 function inserePlacar() {
   var corpoTabela = $(".placar").find("tbody");
   var usuario = "Renato";
@@ -7,6 +9,16 @@ function inserePlacar() {
   linha.find(".botao-remover").click(removeLinha);
 
   corpoTabela.append(linha);
+  $(".placar").slideDown(500);
+  scrollPlacar();
+}
+
+function scrollPlacar() {
+  var posicaoPlacar = $(".placar").offset().top;
+  $("body").animate(
+    {
+      scrollTop: posicaoPlacar+"px"
+  }, 1000);
 }
 
 function novaLinha(usuario, numeroPalavras) {
@@ -34,5 +46,14 @@ function novaLinha(usuario, numeroPalavras) {
 
 function removeLinha() {
   event.preventDefault();
-  $(this).parent().parent().remove();
+  var linha = $(this).parent().parent();
+  linha.fadeOut(1000);
+  setTimeout(function() {
+    linha.remove();
+  }, 1000);
+
+}
+
+function mostraPlacar() {
+    $(".placar").stop().slideToggle(1000);
 }
