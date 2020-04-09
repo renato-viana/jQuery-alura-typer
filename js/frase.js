8a256475345eba1c04e47bc0419360e27ec3e626
Renato Viana <renatoviana30@gmail.com>
2020-04-09 18:03:53 -0300

    Tratando erros e melhorando a experiência

--- js/frase.js
+++ js/frase.js
@@ -1,7 +1,17 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+    $("#spinner").show();
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+    .fail(function() {
+        $("#erro").show();
+        setTimeout(function() {
+            $("#erro").toggle();
+        }, 2000)  
+    })
+    .always(function() {
+        $("#spinner").toggle();
+    });
 }
 
 function trocaFraseAleatoria(data) {
